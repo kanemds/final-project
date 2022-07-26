7f5a9cc7cf0804d77f5f82741ed266ffdcd693f1
Kanem <kanemdeng@email.com>
2022-07-25 18:15:30 -0700

    corrected the right link

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import '../App.css'
 import { SidebarData } from './SidebarData'
+import {
+  Link
+} from "react-router-dom";
+import {
+  useLocation
+} from "react-router-dom";
 
-export const Sidebar = () => {
-      return (
-
-
-        <div className='Sidebar'>
-          <ul className='SidebarList'>
-          { SidebarData.map((icon, key) => {
-            return (
-              <li key={key} onClick={() => {window.location.pathname = icon.link }} className="row" id={window.location.pathname === icon.link ? "active" : ""}> 
+export const Sidebar = (props) => {
+  const location = useLocation();
+  
+  return (
+    <div className='Sidebar'>
+      <ul className='SidebarList'>
+        { SidebarData.map((icon, key) => {
+          return (
+            <li key={key} className="row" data-active={location.pathname.indexOf(icon.link) >= 0 ? "true" : "false"}> 
+              <Link to={icon.link}>
                 <div id="icon">{icon.icon}</div>
                 <div id="title">{icon.title}</div>
-              </li>
-            )
-          }) 
-         }
+              </Link>
+            </li>
+          )
+        }) 
+      }
+      </ul>
+    </div>
+  )
+}
     
-          </ul>
-        </div>
-      )
-    }
-    
-    export default Sidebar
+export default Sidebar
 
 
