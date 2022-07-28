0a79e85f0ce3ffc4a192afd6703ccdffefc7775a
GraceLR <gracerwork08@gmail.com>
2022-07-27 23:33:35 -0400

    change about All of the Above/None of the Above

--- client/src/components/page/Exams/Questions/QuestionForm.js
+++ client/src/components/page/Exams/Questions/QuestionForm.js
@@ -7,14 +7,18 @@ import TextField from "@mui/material/TextField";
 
 import { api_base } from 'config'
 import Answer from './Answer';
+import AllAbove from './AllAbove';
 
 const QuestionForm = () => {
   const [selected, setSelected] = React.useState("");
   const [question, setQuestion] = React.useState("");
   const [answers, setAnswers] = React.useState(['','','','']);
+  const [checked, setChecked] = useState(false);
+	const [above, setAbove] = useState("All");
   const handleChange = (answerId) => {
     setSelected(answerId);
   };
+  const [aboveSelected, setAboveSelected] = React.useState("All of the Above");
   const {id} = useParams();
   const cancelLink = `/exams/${id}/questions`;
   const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
@@ -29,34 +33,28 @@ const QuestionForm = () => {
           label="Type your question here."
         />
         {answers.map((answer, index) => {
+          if (index === answers.length - 1 && checked) {
+            answer = aboveSelected;
+          }
           return (
             <div key={index + 1} style={{display: "flex", flexDirection: "rows"}}>
               <h4>{letters[index]}</h4>
               <Answer setSelected={setSelected} handleChange={handleChange} selected={selected} answerId={index} answer={answer} setAnswers={setAnswers} />
           </div>
           )})}
-        <Button onClick={() => setAnswers(prev => {
-          const newPrev = [];
-          prev.forEach((ans, i) => {
-            if (i === prev.length - 1) {
-              if (ans === "All of the Above") {
-                newPrev.push("");
-                newPrev.push("All of the Above");
-              } else {
-                newPrev.push(ans);
-                newPrev.push("");
-              }
+        <Button onClick={() => {
+          setAnswers(prev => {
+            const newPrev = [...prev];
+            if (checked) {
+              newPrev[newPrev.length - 1] = "";
+              newPrev.push(aboveSelected);
             } else {
-              newPrev.push(ans);
+              newPrev.push("");
             }
-          });
-          return newPrev;
-        })} disabled={answers.length >= 6}>Add Choice</Button>
-        <Button onClick={() => setAnswers(prev => {
-          const newPrev = [...prev];
-          newPrev.push("All of the Above");
-          return newPrev;
-        })} disabled={answers.length >= 6}>Add {letters[answers.length]}: All of the Above</Button>
+            return newPrev;
+          })
+        }} disabled={answers.length >= 6}>Add Choice</Button>
+        <AllAbove letter={letters[answers.length - 1]} checked={checked} setChecked={setChecked} setAboveSelected={setAboveSelected}/>
         <Button component={Link} to={cancelLink}>Cancel</Button>
         <Button onClick={() => alert(`${selected} - ${question} - ${answers}`)}>Save</Button>
       </div>
