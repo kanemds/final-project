489aa8cc61736ed49bf921d6930b68a98542ec1b
Kanem <kanemdeng@email.com>
2022-08-09 14:16:54 -0700

    update again

--- client/src/components/page/Exams/useExam.js
+++ client/src/components/page/Exams/useExam.js
@@ -3,24 +3,6 @@ import { api_base } from 'config'
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-function shuffle(array) {
-  let currentIndex = array.length, randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
-
 function useExam() {
   const [exam, setExam] = useState([]);
   const params = useParams()
@@ -34,22 +16,6 @@ function useExam() {
     setExam(exam.data)
   }
 
-  /* post /exam/:id/questions/:questionId
-  // body { answer: answer_id }
-  // ????
-  // new model, scorecard
-  // {
-    studentId: ____,
-    examId: ____
-    answers: [{
-      userChoice: 0, // index of answer or id of answer
-      isCorrectAnswer: true or false
-    }],
-    score: 5, // calculated from [total number of isCorrectAnsswe] / total questions,
-    attempt:
-    date:
-    limit:
-  */
 
   const saveExam = async (exam) => {
     // do save exam
